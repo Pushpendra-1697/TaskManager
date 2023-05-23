c69819c6d6fec8053fbb80624910deffedc8f64f
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2023-05-23 10:55:34 +0530

    added readme file

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <Box textAlign={"center"} display="flex" justifyContent={"center"} alignItems="center" flexDirection={"column"}>
         <Heading mb="10px">Welcome to Task Manager App</Heading>
-        <Img height={"400px"} w="500px" src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-C0J1czMXGlvhiDr1qbTXq74AILp2uspNPw&usqp=CAU' alt='bug profile' />
+        <Img height={"400px"} w="500px" src='https://media.istockphoto.com/id/1056305928/photo/checking-completed-tasks-on-a-list.jpg?b=1&s=170667a&w=0&k=20&c=9dTbaI6J1kizUwLgrPTDs7fpMVsxOqzBe6YKTerJxSY=' alt='bug profile' />
     </Box>
   );
 }
